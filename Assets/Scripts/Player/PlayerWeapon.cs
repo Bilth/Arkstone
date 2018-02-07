88d34e1485ea59ffb7a42e2c01a9286aa049620d
Brian Mayzak <bmayzak@gmail.com>
2018-02-07 11:02:30 -0500

    New

--- Assets/Scripts/Player/PlayerWeapon.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-// Allows it to show up as a variable in the inspect
-[System.Serializable]
-public class PlayerWeapon : MonoBehaviour {
-    public string weaponName = "Wand";
-    public int damage = 50;
-    public float range = 200f;
-}
