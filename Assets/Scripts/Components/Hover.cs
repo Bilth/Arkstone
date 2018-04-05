284bbec6e62b54422790bdb9c763d637f8638c86
Brian Mayzak <bmayzak@gmail.com>
2018-04-05 19:27:54 -0400

    PID + Floating Eye are IN!

---
+++ Assets/Scripts/Components/Hover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    public float Distance = 3f;
+    public PID Controller;
+
+    public Transform Sensor;
+    public LayerMask GroundMask;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (!Physics.Raycast(Sensor.position, -Sensor.up, out hit, Distance * 2, GroundMask))
+            return;
+
+        var force = Mathf.Max(0, Controller.Update(Distance, hit.distance));
+        rb.AddForce(transform.up * force);
+        Debug.Log("Force: " + force + ", Hit Distance: " + hit.distance);
+    }
+}
---
+++ Assets/Scripts/Helper/PID.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PID
+{
+    public float P, I, D;
+
+    float integral;
+    float lastError;
+
+    public float Update(float setPoint, float position)
+    {
+        float error = setPoint - position;
+        integral += error * Time.deltaTime;
+
+        float derivative = (error - lastError) / Time.deltaTime;
+        lastError = error;
+
+        return error * P + integral * I + derivative * D;
+    }
+}
