7cbc4b41432f6e237fb023715f02a4b5810ccd81
Brian Mayzak <bmayzak@gmail.com>
2018-04-05 23:40:54 -0400

    Added Movers

---
+++ Assets/Scripts/Components/Mover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+
+    public Vector3 startingVelocity = Vector3.zero;
+
+    private Rigidbody _body;
+
+    void Start()
+    {
+        _body = GetComponent<Rigidbody>();
+
+        Vector3 tVelocity = _body.velocity;
+        _body.velocity = startingVelocity;
+    }
+
+    void Update()
+    {
+       // transform.Rotate(new Vector3(xPerSecond * Time.deltaTime, yPerSecond * Time.deltaTime, zPerSecond * Time.deltaTime));
+    }
+}
+
---
+++ Assets/Scripts/Components/PIDMover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PIDMover : MonoBehaviour
+{
+    public float Distance = 3f;
+    public PID Controller;
+
+    public Transform Sensor;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+       // var force = Mathf.Max(0, Controller.Update(Distance, hit.distance));
+       // rb.AddForce(transform.up * force);
+    }
+}
